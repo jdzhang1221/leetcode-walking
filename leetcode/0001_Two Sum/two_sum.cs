6b0cf9ae743d3530cba946b9b0cb3924b5717e0f
jdzhang <673235694@qq.com>
2019-05-28 22:35:22 +0800

    添加项目文件。

---
+++ leetcode/0001_Two Sum/two_sum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace leetcode._0001_Two_Sum
+{
+    public class two_sum
+    {
+        /// <summary>
+        /// 时间复杂度 O（n^2）  空间复杂度O（1）
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+        /// <summary>
+        /// 时间复杂度O（n） 空间复杂度O（n）
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum1(int[] nums, int target)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (keyValuePairs.ContainsKey(complement))
+                {
+                    return new int[] { keyValuePairs[complement], i };
+                }
+                if (!keyValuePairs.ContainsKey(nums[i]))
+                {
+                    keyValuePairs.Add(nums[i], i);
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ leetcode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace leetcode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Two_Sum
+            int[] nums = new int[] { 2, 7, 11, 15 };
+            int target = 9;
+            var result = _0001_Two_Sum.two_sum.TwoSum(nums, target);
+            var result1 = _0001_Two_Sum.two_sum.TwoSum1(nums, target);
+            #endregion
+            Console.Read();
+        }
+    }
+}
