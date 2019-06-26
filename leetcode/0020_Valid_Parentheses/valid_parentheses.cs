bbb3c1a6e461326fc31d9802078f5307bc7554ec
jdzhang <673235694@qq.com>
2019-06-26 20:35:40 +0800

    add comment

--- leetcode/0020_Valid_Parentheses/valid_parentheses.cs
+++ leetcode/0020_Valid_Parentheses/valid_parentheses.cs
@@ -36,6 +36,7 @@ namespace leetcode._0020_Valid_Parentheses
          */
     public class valid_parentheses
     {
+        // 初始化容器，存储 右边抵消的字符
         private static Dictionary<char, char> keyValuePairs;
         static valid_parentheses()
         {
@@ -51,20 +52,25 @@ namespace leetcode._0020_Valid_Parentheses
         /// <returns></returns>
         public static bool IsValid(string s)
         {
+            //栈：先进后出;
             Stack<char> stack = new Stack<char>();
             for (int i = 0; i < s.Length; i++)
             {
                 char one = s[i];
+                //判断当前字符是否需要抵消
                 if (keyValuePairs.ContainsKey(one))
                 {
-                    char element = stack.Count == 0 ? '#' : stack.Pop();
-                    if (element != keyValuePairs[one])
+                    //获取栈顶的第一个字符
+                    char topElement = stack.Count == 0 ? '#' : stack.Pop();
+                    //如果抵消字符和栈顶第一个元素不匹配 直接返回false
+                    if (topElement != keyValuePairs[one])
                     {
                         return false;
                     }
                 }
                 else
                 {
+                    //如果字符是开区间，就把字符压入栈中
                     stack.Push(s[i]);
                 }
             }
